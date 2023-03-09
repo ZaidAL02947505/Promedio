9109d755a872788115a850fb0ffa799433dd89b1
ZaidAL02947505 <127364921+ZaidAL02947505@users.noreply.github.com>
2023-03-08 20:34:07 -0600

    Add files via upload

---
+++ Promedio.java
@@ -0,0 +1,77 @@
+package arreglo.promedio;
+
+import java.util.Scanner;
+
+public class Promedio {
+    
+    public static void main (String[]args){
+        Scanner s=new Scanner(System.in);
+        int calificaciones[]=new int[5];
+        int suma=0;
+        float promedio=0;
+        String nombre;
+        
+        System.out.println("Por favor ingresa tu nombre");
+        nombre=s.nextLine();
+        
+        System.out.println("\n"+"Por favor ingresa tus calificaiones"+"\n");
+        
+        System.out.println("Materia 1:");
+        calificaciones[0]=s.nextInt();
+        
+        System.out.println("Materia 2:");
+        calificaciones[1]=s.nextInt();
+        
+        System.out.println("Materia 3:");
+        calificaciones[2]=s.nextInt();
+        
+        System.out.println("Materia 4:");
+        calificaciones[3]=s.nextInt();
+        
+        System.out.println("Materia 5:");
+        calificaciones[4]=s.nextInt();
+        
+        System.out.println("\n");
+        for(int i=0; i<calificaciones.length; i++){
+            suma= suma + calificaciones[i];
+            
+        promedio= suma / calificaciones.length;
+        
+            
+        }  
+        
+        System.out.println("Alumno:"+nombre+"\n");
+        System.out.println("Calificacion 1: "+calificaciones[0]);
+        System.out.println("Calificacion 2: "+calificaciones[1]);
+        System.out.println("Calificacion 3: "+calificaciones[2]);
+        System.out.println("Calificacion 4: "+calificaciones[3]);
+        System.out.println("Calificacion 5: "+calificaciones[4]+"\n");
+        System.out.println("Tu promedio es: "+promedio);
+        
+    
+        if (promedio <= 50)
+            System.out.println("Calificacion: F");
+        
+        if (promedio >=51 && promedio <=60)
+            System.out.println("Calificacion: E"); 
+        
+        if (promedio >=61 && promedio <=70)
+            System.out.println("Calificacion: D"); 
+        
+        if (promedio >=71 && promedio <=80)
+            System.out.println("Calificacion: C");
+        
+        if (promedio >=81 && promedio <=90)
+            System.out.println("Calificacion: B");
+        
+        if (promedio >=91 && promedio <=100)
+            System.out.println("Calificacion: A");
+        
+        if (promedio<=99)
+            System.out.println("\n"+nombre+ ", no te des por vencido, puedes ser mejor.");
+        
+        if (promedio >=100)
+            System.out.println("\n"+"Felicidades "+nombre+", obtuviste una calificacion perfecta.");
+    }
+        
+}    
